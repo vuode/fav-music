9a466963e6c5880ca8c188b6d15c78d9b03b8c83
Ilia Byzov <17595195+vuode@users.noreply.github.com>
2022-05-15 03:17:06 +0200

    Add util for saving and loading localStorage data

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,47 @@
+export enum SaveStateStatus {
+  Success = 'success',
+  Error = 'error',
+}
+
+/**
+ * Function to load localStorage item, stored as serialized JSON
+ * @param name localStorage key, where the data is stored
+ * @returns parsed localStorage item if it was sored as JSON or undefined
+ */
+export const loadState = (name: string) => {
+  try {
+    const serializedState = localStorage.getItem(name);
+
+    if (serializedState === null) {
+      return undefined;
+    }
+
+    const state = JSON.parse(serializedState);
+
+    return state;
+  } catch (error) {
+    return undefined;
+  }
+};
+
+/**
+ * Function to save objects and arrays to localStorage as serialized JSON
+ * @param name localStorage key, where the data will be stored
+ * @param state data to store
+ * @returns status of operation
+ */
+export const saveState = <T>(name: string, state: T) => {
+  try {
+    if (typeof state !== 'object') {
+      return SaveStateStatus.Error;
+    }
+
+    const serializedState = JSON.stringify(state);
+
+    localStorage.setItem(name, serializedState);
+
+    return SaveStateStatus.Success;
+  } catch (err) {
+    return SaveStateStatus.Error;
+  }
+};
