e775a20ed31be26549191d21c2ace47cf3f25b62
Ilia Byzov <17595195+vuode@users.noreply.github.com>
2022-05-15 08:38:13 +0200

    Add App and Input components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useAppDispatch } from '../../hooks/redux';
+
+import { addAlbum } from '../../store/features/albums/albumsSlice';
+
+import List from '../List';
+import Input from '../Input';
+
+import styles from './App.module.css';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const dispatch = useAppDispatch();
+
+  const [input, setInput] = useState('');
+
+  return (
+    <main className={styles.app}>
+      <form className={styles.form} onSubmit={() => dispatch(addAlbum(input))}>
+        <Input
+          value={input}
+          onChange={(event) => setInput(event.currentTarget.value)}
+        />
+        <button className={styles.button} type='submit'>
+          Add album
+        </button>
+      </form>
+      <List />
+    </main>
+  );
 };
 
 export default App;
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,22 @@
+import React, { ChangeEvent } from 'react';
+
+import styles from './Input.module.css';
+
+type Props = {
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+};
+
+const Input = ({ value, onChange }: Props) => {
+  return (
+    <input
+      className={styles.input}
+      type='text'
+      required
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
+
+export default Input;
